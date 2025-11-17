4a62ce2e196e86c53e0a02033edc8d71df1a1fc5
dkaffe <dkaffe@outlook.de>
2025-11-17 08:58:02 +0100

    Update phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // Filter component
 const Filter = ({ filterName, handleFilterChange }) => {
@@ -47,15 +48,17 @@ const Persons = ({ personsToShow }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  // Fetch database entries
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   // Handlers for state & logic
   const handleNameChange = (event) => {
@@ -94,7 +97,6 @@ const App = () => {
       number: trimmedNumber,
       id: persons.length + 1,
     };
-    console.log(personObject);
 
     setPersons(persons.concat(personObject));
     setNewName("");
