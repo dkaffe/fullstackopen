042ca256ce74b4699ba708aad093a0b9a69fa5bf
dkaffe <dkaffe@outlook.de>
2025-11-18 08:15:28 +0100

    Reconfigure backend

--- part3/webserver/index.js
+++ part3/webserver/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
+
 
 let notes = [
   {
@@ -74,6 +78,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
